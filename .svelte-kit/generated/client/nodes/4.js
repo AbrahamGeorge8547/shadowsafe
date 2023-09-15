43fd6093aa3685ae577e666a345b5c08fb5d406b
abrahamgeorge8547 <abrahamgeorge8547@gmail.com>
2023-09-15 09:44:15 +0530

    Untrack .svelte-kit folder

--- .svelte-kit/generated/client/nodes/4.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/people/[slug]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/people/[slug]/+page.svelte";
--- .svelte-kit/types/src/routes/api/secrets/[secretId]/$types.d.ts
+++
@@ -1,9 +0,0 @@
-import type * as Kit from '@sveltejs/kit';
-
-type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
-type RouteParams = { secretId: string }
-type RouteId = '/api/secrets/[secretId]';
-
-export type EntryGenerator = () => Promise<Array<RouteParams>> | Array<RouteParams>;
-export type RequestHandler = Kit.RequestHandler<RouteParams, RouteId>;
-export type RequestEvent = Kit.RequestEvent<RouteParams, RouteId>;
