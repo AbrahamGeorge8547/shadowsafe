909182ecdaa7061fecdad08364047dc32c03d0f0
Harikrishnan P R <harikrishnan.pr@hatio.in>
2023-09-15 11:31:32 +0530

    Update people ui

--- src/routes/vault/people/+page.svelte
+++ src/routes/vault/people/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { TreeView, FoldersView } from "$lib/components";
+  import { TreeView, FoldersView } from '$lib/components';
   import {
     treeStore,
     navigationHistory,
@@ -7,44 +7,48 @@
     selectedNodeChildren,
     breadCrumbs,
     currentParentNode,
-  } from "$lib/store/ui";
-  import { peopleList } from "$lib/store";
-  import { BreadCrumbs, SecretsCard } from "$lib/components";
-  import { findNodeById, findParentNodesById } from "$lib/util";
-  import Icon from "@iconify/svelte";
-  import { PeopleListView } from "$lib/components";
-  import People from "$lib/components/people.svelte";
+  } from '$lib/store/ui';
+  import { peopleList } from '$lib/store';
+  import { BreadCrumbs, SecretsCard } from '$lib/components';
+  import { createNewFolder } from '$lib/util/drawerSettings';
+  import { findNodeById, findParentNodesById } from '$lib/util';
+  import Icon from '@iconify/svelte';
+  import { PeopleListView } from '$lib/components';
+  import People from '$lib/components/people.svelte';
+  import { getDrawerStore } from '@skeletonlabs/skeleton';
+  const drawerStore = getDrawerStore();
+  let currentNode;
   const tree = {
     id: 1,
-    label: "Groups",
+    label: 'Groups',
     children: [
       {
         id: 2,
         parentId: 1,
-        label: "Team1",
+        label: 'Team1',
         children: [
-          { id: 3, parentId: 2, label: "dev" },
+          { id: 3, parentId: 2, label: 'dev' },
           {
             id: 4,
-            label: "testers",
+            label: 'testers',
             parentId: 2,
             children: [
-              { id: 6, parentId: 4, label: "Admin" },
-              { id: 8, parentId: 4, label: "corporate-admin" },
-              { id: 7, parentId: 4, label: "spenders" },
+              { id: 6, parentId: 4, label: 'Admin' },
+              { id: 8, parentId: 4, label: 'corporate-admin' },
+              { id: 7, parentId: 4, label: 'spenders' },
             ],
           },
-          { id: 5, parentId: 2, label: "qa" },
+          { id: 5, parentId: 2, label: 'qa' },
         ],
       },
       {
         parentId: 1,
         id: 9,
-        label: "Team2",
+        label: 'Team2',
         children: [
-          { id: 10, parentId: 9, label: "devs" },
-          { id: 11, parentId: 9, label: "testers" },
-          { id: 12, parentId: 9, label: "qa" },
+          { id: 10, parentId: 9, label: 'devs' },
+          { id: 11, parentId: 9, label: 'testers' },
+          { id: 12, parentId: 9, label: 'qa' },
         ],
       },
     ],
@@ -79,7 +83,7 @@
   function handleDrop(event) {
     event.preventDefault();
 
-    const personData = event.dataTransfer.getData("person");
+    const personData = event.dataTransfer.getData('person');
     const person = JSON.parse(personData);
     peopleList.update((people) => {
       return [...people, person];
@@ -88,14 +92,20 @@
     // Now `person` is available to be added to your folder or whatever structure you have
     // console.log("Dropped:", person);
   }
+
+  treeStore.set(tree);
+  $: {
+    currentNode = findNodeById($treeStore, $currentParentNode);
+  }
+
+  const addNewFolder = async () => {
+    // @ts-ignore
+    drawerStore.open(createNewFolder);
+  };
 </script>
 
 <div class="bread-crumbs-container flex items-center ml-4">
-  <button
-    type="button"
-    class="btn-icon btn-icon-sm variant-filled-tertiary m-4"
-    on:click={goBack}
-  >
+  <button type="button" class="btn-icon btn-icon-sm variant-filled-tertiary m-4" on:click={goBack}>
     <Icon icon="ep:back" />
   </button>
   <div class="flex-box card-hover variant-outline-tertiary rounded-md p-1">
@@ -115,20 +125,32 @@
   <div class="flex flex-col flex-grow">
     <div class="folders-area flex-grow flex">
       <div
-        class="folders-area flex-grow border-4 border-dashed border-gray-300"
+        class="folders-area flex-grow border-2 border-[#235789] rounded-xl mx-8 min-w-0"
         on:drop={handleDrop}
         on:dragover={allowDrop}
       >
-        <FoldersView />
-        <ul class="list">
-          {#each $peopleList as people}
-            <li class="card ml-2 p-4">
-              {people.username}
-            </li>
-          {/each}
-        </ul>
+        <!-- <FoldersView /> -->
+        <div class="flex flex-row justify-between px-8 items-center py-6">
+          <div>
+            <h1>{`${currentNode ? currentNode.label : ''}`}</h1>
+          </div>
+          <div class="flex flex-row">
+            <button class="bg-[#4E46DC] px-3 py-1.5 rounded-2xl ml-4" on:click={addNewFolder}
+              >Add Folder</button
+            >
+          </div>
+        </div>
+        <div class=" py-4 border-t-2 rounded-xl border-[#235789]">
+          <ul class="list">
+            {#each $peopleList as people}
+              <li class="card ml-2 p-4">
+                {people.username}
+              </li>
+            {/each}
+          </ul>
+        </div>
       </div>
-      <div class="flex-grow">
+      <div class="flex-grow mr-8 max-w-[400px]">
         <PeopleListView />
       </div>
     </div>
--- src/routes/vault/secrets/+page.svelte
+++ src/routes/vault/secrets/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { TreeView } from "$lib/components";
+  import { TreeView } from '$lib/components';
   import {
     treeStore,
     navigationHistory,
@@ -7,49 +7,46 @@
     selectedNodeChildren,
     breadCrumbs,
     currentParentNode,
-  } from "$lib/store/ui";
-  import {
-    createNewFolder,
-    createSecretDrawerSettings,
-  } from "$lib/util/drawerSettings";
-  import { BreadCrumbs, SecretsCard } from "$lib/components";
-  import { findNodeById, findParentNodesById } from "$lib/util";
-  import Icon from "@iconify/svelte";
-  import { getDrawerStore } from "@skeletonlabs/skeleton";
+  } from '$lib/store/ui';
+  import { createNewFolder, createSecretDrawerSettings } from '$lib/util/drawerSettings';
+  import { BreadCrumbs, SecretsCard } from '$lib/components';
+  import { findNodeById, findParentNodesById } from '$lib/util';
+  import Icon from '@iconify/svelte';
+  import { getDrawerStore } from '@skeletonlabs/skeleton';
   const drawerStore = getDrawerStore();
 
   let currentNode;
   const tree = {
     id: 1,
-    label: "VAULT",
+    label: 'VAULT',
     children: [
       {
         id: 2,
         parentId: 1,
-        label: "UAT",
+        label: 'UAT',
         children: [
-          { id: 3, parentId: 2, label: "DB" },
+          { id: 3, parentId: 2, label: 'DB' },
           {
             id: 4,
-            label: "USERNAMES",
+            label: 'USERNAMES',
             parentId: 2,
             children: [
-              { id: 6, parentId: 4, label: "Admin" },
-              { id: 8, parentId: 4, label: "corporate-admin" },
-              { id: 7, parentId: 4, label: "spenders" },
+              { id: 6, parentId: 4, label: 'Admin' },
+              { id: 8, parentId: 4, label: 'corporate-admin' },
+              { id: 7, parentId: 4, label: 'spenders' },
             ],
           },
-          { id: 5, parentId: 2, label: "KAFKA" },
+          { id: 5, parentId: 2, label: 'KAFKA' },
         ],
       },
       {
         parentId: 1,
         id: 9,
-        label: "STAGE",
+        label: 'STAGE',
         children: [
-          { id: 10, parentId: 9, label: "DB" },
-          { id: 11, parentId: 9, label: "USER NAMES" },
-          { id: 12, parentId: 9, label: "KAFKA" },
+          { id: 10, parentId: 9, label: 'DB' },
+          { id: 11, parentId: 9, label: 'USER NAMES' },
+          { id: 12, parentId: 9, label: 'KAFKA' },
         ],
       },
     ],
@@ -91,11 +88,7 @@
 </script>
 
 <div class="bread-crumbs-container flex items-center ml-4">
-  <button
-    type="button"
-    class="btn-icon btn-icon-sm variant-filled-tertiary m-4"
-    on:click={goBack}
-  >
+  <button type="button" class="btn-icon btn-icon-sm variant-filled-tertiary m-4" on:click={goBack}>
     <Icon icon="ep:back" />
   </button>
   <div class="flex-box card-hover variant-outline-tertiary rounded-md p-1">
@@ -107,7 +100,7 @@
     <input
       type="search"
       class="variant-filled-surface border-0 rounded-l-lg"
-      placeholder="search..."
+      placeholder="Search..."
       id="search-input"
       on:change={(text) => {
         console.log(text);
@@ -128,21 +121,17 @@
   </div>
 
   <!-- FoldersView on the right -->
-  <div
-    class="flex flex-col flex-grow h-4/5 border-2 border-[#235789] rounded-xl mx-8"
-  >
+  <div class="flex flex-col flex-grow h-4/5 border-2 border-[#235789] rounded-xl mx-8">
     <div class="flex flex-row justify-between px-8 items-center py-6">
       <div>
-        <h1>{`${currentNode ? currentNode.label : ""}`}</h1>
+        <h1>{`${currentNode ? currentNode.label : ''}`}</h1>
       </div>
       <div class="flex flex-row">
-        <button
-          class="bg-[#4E46DC] px-3 py-1.5 rounded-2xl"
-          on:click={createSecret}>Add Secret</button
+        <button class="bg-[#4E46DC] px-3 py-1.5 rounded-2xl" on:click={createSecret}
+          >Add Secret</button
         >
-        <button
-          class="bg-[#4E46DC] px-3 py-1.5 rounded-2xl ml-4"
-          on:click={addNewFolder}>Add Folder</button
+        <button class="bg-[#4E46DC] px-3 py-1.5 rounded-2xl ml-4" on:click={addNewFolder}
+          >Add Folder</button
         >
       </div>
     </div>
