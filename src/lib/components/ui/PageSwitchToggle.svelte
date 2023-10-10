0d84a1dd4bf72c83cb884e4137c44468982c1ac5
Harikrishnan P R <harikrishnan.pr@hatio.in>
2023-10-10 20:50:38 +0530

    Toggle button ui update

--- src/lib/components/ui/PageSwitchToggle.svelte
+++ src/lib/components/ui/PageSwitchToggle.svelte
@@ -1,22 +1,36 @@
 <script>
-    import { goto } from "$app/navigation";
-    import { RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
-    let value = "Secrets";
-    $: if (!import.meta.env.SSR) {
-        // Check if running on client
-        if (value === "Secrets") {
-            goto("/vault/secrets");
-        } else if (value === "Groups") {
-            goto("/vault/people");
-        }
+  import { goto } from "$app/navigation";
+  import { RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
+  let value = "Secrets";
+  $: if (!import.meta.env.SSR) {
+    // Check if running on client
+    if (value === "Secrets") {
+      goto("/vault/secrets");
+    } else if (value === "Groups") {
+      goto("/vault/people");
     }
+  }
 </script>
 
-<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
-    <RadioItem bind:group={value} name="justify" value="Secrets"
-        >Secrets</RadioItem
+<RadioGroup
+  active="bg-[#4C598B]"
+  hover="hover:variant-soft-primary"
+  padding="px-4 py-1"
+  background="bg-[#262C44]"
+>
+  <RadioItem bind:group={value} name="justify" value="Secrets" id="secrets">
+    <label for="secrets" class={value === "Secrets" ? "text-[#fff]" : "text-[#828CAE]"}
+      >Secrets</label
     >
-    <RadioItem bind:group={value} name="justify" value="Groups"
-        >Groups</RadioItem
-    >
+  </RadioItem>
+  <RadioItem bind:group={value} name="justify" value="Groups" id="groups">
+    <label for="groups" class={value === "Groups" ? "text-[#fff]" : "text-[#828CAE]"}>Groups</label>
+  </RadioItem>
 </RadioGroup>
+
+<style>
+  /* Custom style for unselected labels */
+  .unselected-label {
+    color: #666; /* Replace with your desired text color for unselected labels */
+  }
+</style>
