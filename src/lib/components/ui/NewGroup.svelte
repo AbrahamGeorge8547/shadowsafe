a6bb58a49c351158d16bc4be31778145ed9b4199
TONY ANTONY <tonyantony300@gmail.com>
2023-10-15 21:51:29 +0530

    models placed at center- save btn changed

--- src/lib/components/ui/NewGroup.svelte
+++ src/lib/components/ui/NewGroup.svelte
@@ -1,10 +1,12 @@
 <script>
   import { groupList } from "$lib/store/people";
   import { getDrawerStore } from "@skeletonlabs/skeleton";
+  import Icon from "@iconify/svelte";
   const drawerStore = getDrawerStore();
   let folderName;
   const addNewFolder = async (e) => {
     if (folderName && e.key == "Enter") {
+      console.log('Enter pressed')
       await fetch("/api/groups", {
         method: "POST",
         body: JSON.stringify({
@@ -21,14 +23,20 @@
   };
 </script>
 
-<div class="container mx-auto p-4 relative">
-  <label class="label mb-2">
-    Group Name :
-    <input
-      class="input"
-      type="text"
-      bind:value={folderName}
-      on:keydown={addNewFolder}
-    />
-  </label>
+
+
+<div class="container h-full mx-auto p-4 relative bg-[#2E3654] flex flex-col justify-between ">
+  <div class="text-3xl flex justify-between">
+    <h1>Add Group</h1>
+    <Icon icon="iconamoon:close-bold" class="" color="#4C598B"/>
+  </div>
+  <input
+    class="input !bg-[#2E3654]"
+    type="text"
+    placeholder="Group"
+    bind:value={folderName}
+    on:keydown={addNewFolder}
+  />
+  <button on:click={addNewFolder} class="bg-[#4E46DC] rounded-3xl w-1/2 py-2 px-2 text-md self-center">Add Group</button>
 </div>
+
