972bf0fcaee877b46441fe4707d955c9f6009ee6
Abe <abrahamgeorge8547@gmail.com>
2023-10-16 02:02:58 +0530

    refresh state maintained

--- src/lib/store/ui.ts
+++ src/lib/store/ui.ts
@@ -4,13 +4,13 @@ import { localStorageStore } from "@skeletonlabs/skeleton";
 export const isEditing = writable(false);
 export const selectedNodeChildren = writable([]);
 export const navigationHistory = writable([]);
-export const currentParentNode = writable(null);
+export const currentParentNode = localStorageStore("currentParentNode", null);;
 export const treeStore = writable({});
 export const breadCrumbs = writable([]);
 export const expandedNodes = writable(new Set());
 export const secretFields = writable([]);
 export const editMembers = writable(false);
-export const selectedGroup = writable('AllUsers');
+export const selectedGroup = localStorageStore("selectedGroup", 'AllUsers');
 export const selectedTab = writable('secrets');
 export const droppedItem = writable({});
 export const selectedPermission = writable(null)
