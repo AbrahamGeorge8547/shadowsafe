0db6171fd48ede4efac2151b31e3a6cdc736b7bf
Abe <abrahamgeorge8547@gmail.com>
2023-10-16 17:46:46 +0530

    drag and drop fix

--- src/lib/components/people/PeopleListView.svelte
+++ src/lib/components/people/PeopleListView.svelte
@@ -22,8 +22,12 @@
   }
 </script>
 
-<div class="flex w-full h-[50px] border-0 justify-center items-center !border-b-0">
-  <div class="w-full flex rounded-full !h-[40px] searchWrapper justify-center items-center border border-[#4C598B4D] mx-2 my-2 !bg-[#262C44]">
+<div
+  class="flex w-full h-[50px] border-0 justify-center items-center !border-b-0"
+>
+  <div
+    class="w-full flex rounded-full !h-[40px] searchWrapper justify-center items-center border border-[#4C598B4D] mx-2 my-2 !bg-[#262C44]"
+  >
     <input
       type="search"
       class="variant-filled-surface border-0 !h-[26px] rounded-l-full !bg-[#262C44] flex-1 font-light text-sm"
@@ -31,11 +35,15 @@
       id="search-input"
       bind:value={search}
     />
-    <div class="flex items-center bg-[#262C44] rounded-r-full px-3 justify-center">
+
+    <div
+      class="flex items-center bg-[#262C44] rounded-r-full px-3 justify-center"
+    >
       <Icon icon="ic:baseline-search" class="h-6 w-6" color="#4C598B" />
     </div>
   </div>
 </div>
+<h2 class="ml-4 mb-2 text-3xl">Drag and Drop to give access</h2>
 <ul class="w-full !text-xs !font-light bg-[#2E3654]">
   {#each filteredPeoples as person}
     <li
@@ -43,15 +51,14 @@
       on:dragstart={(e) => handleDragStart(e, person)}
       class="px-2 py-3 !bg-[#262C44] w-full flex border-t-[1px] border-[#374165]"
     >
-    <span class="rounded-full w-[24px] h-[24px] flex justify-center items-center bg-[#4C598B33] mx-2">
-      <Icon
-        icon="tabler:user"
-        class="text-3xl h-[15px] w-[15px]"
-      />
-    </span>
-    <span class="flex justify-center items-center">
-      {person.name}
-    </span>
+      <span
+        class="rounded-full w-[24px] h-[24px] flex justify-center items-center bg-[#4C598B33] mx-2"
+      >
+        <Icon icon="tabler:user" class="text-3xl h-[15px] w-[15px]" />
+      </span>
+      <span class="flex justify-center items-center">
+        {person.name}
+      </span>
     </li>
   {/each}
 </ul>
