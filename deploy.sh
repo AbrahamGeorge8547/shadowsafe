31d4ca81aeab6884e156ddc51e20bf43f431f5b7
akash1729 <akashthomasjohn@gmail.com>
2023-10-26 18:38:18 +0530

    added deploy file with ssh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,19 @@
 
 
-git pull
+ssh ubuntu@13.127.6.232 << EOF
 
-sudo docker rmi osvauld-web:latest
+    cd osvauld-web/
 
-sudo docker build -t osvauld-web:latest .
+    git pull
 
-sudo docker run -d -p 80:3000 --name osvauldweb osvauld-web:latest
+    sudo docker stop osvauldweb
+
+    sudo docker rm osvauldweb
+
+    sudo docker rmi osvauld-web:latest
+
+    sudo docker build -t osvauld-web:latest .
+
+    sudo docker run -d -p 80:3000 --name osvauldweb osvauld-web:latest
+EOF
+
